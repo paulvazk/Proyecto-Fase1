350524d1d24f2c3b01790313d0e25887eb32918b
Paul Vazquez Cervantes <paulvazk@protonmail.com>
2020-08-03 21:39:05 -0500

    Funcion limpiar campos

---
+++ proyecto/vistas/js/usuarios.js
@@ -0,0 +1,25 @@
+/**
+ * Se genera las funciones dinamicas con Jquery
+ */
+var tabla;
+     //Primera Funcion que se ejecuta
+     function init() {
+          //Va a servir de referancia para llamar a los demas
+     }
+     //Funcion que limpia los campos del formulario 
+     function limpiar() {
+          $("#cedula").val("");
+          $("#nombre").val("");
+          $("#apellido").val("");
+          $("#cargo").val("");
+          $("#usuario").val("");
+          $("#password1").val("");
+          $("#password2").val("");
+          $("#telefono").val("");
+          $("#email").val("");
+          $("#direccion").val("");
+          $("#estado").val("");
+          $("#id_usuario").val("");
+     }
+     //Llamando a la funcion
+     init();
